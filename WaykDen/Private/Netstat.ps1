5e82a0a1a0b9bb64f1da148b0d846bc674b0f578
Marc-André Moreau <mamoreau@devolutions.net>
2020-08-20 09:57:47 -0400

    WaykDen-ps: add Windows netstat wrapper

--- WaykDen/Private/Netstat.ps1
+++ WaykDen/Private/Netstat.ps1
@@ -2,30 +2,27 @@
 function Get-Netstat
 {
     if ($IsLinux) {
-        $output = netstat -an --tcp | grep LISTEN
-
         # Linux netstat:
         # Proto Recv-Q Send-Q Local Address           Foreign Address         State
         # tcp        0      0 127.0.0.53:53           0.0.0.0:*               LISTEN
         # tcp        0      0 0.0.0.0:22              0.0.0.0:*               LISTEN
         # tcp        0      0 127.0.0.1:631           0.0.0.0:*               LISTEN
 
+        $output = netstat -an --tcp | grep LISTEN
+
         foreach ($line in $output) {
-            $line = $line -Replace '^\s+', ''
-            $line = $line -Split '\s+'
+            $line = $line.Trim()
+            $line = $line -Split '\s+' # split line by whitespace
             $Protocol = $line[0]
-            $RecvQueue = $line[1]
-            $SendQueue = $line[2]
             $LocalAddress = $line[3]
             $ForeignAddress = $line[4]
             $State = $line[5]
 
-            $LocalPort = $($LocalAddress -Split ':')[-1] # Linux uses ':' separator for port
+            # Linux uses ':' separator for port
+            $LocalPort = $($LocalAddress -Split ':')[-1]
 
             New-Object -TypeName PSObject -Property @{
                 Protocol = $Protocol
-                RecvQueue = $RecvQueue
-                SendQueue = $SendQueue
                 LocalAddress = $LocalAddress
                 LocalPort = $LocalPort
                 ForeignAddress = $ForeignAddress
@@ -34,29 +31,27 @@ function Get-Netstat
         }
     }
     elseif ($IsMacOS) {
-        $output = netstat -an -p tcp | grep LISTEN
-
         # macOS netstat:
         # Proto Recv-Q Send-Q  Local Address          Foreign Address        (state)
         # tcp46      0      0  *.8080                 *.*                    LISTEN
         # tcp4       0      0  127.0.0.1.631          *.*                    LISTEN
 
+        $output = netstat -an -p tcp | grep LISTEN
+
         foreach ($line in $output) {
-            $line = $line -Replace '^\s+', '' # trim whitespace at the beginning of line
+            $line = $line.Trim()
             $line = $line -Split '\s+' # split line by whitespace
             $Protocol = $line[0]
-            $RecvQueue = $line[1]
-            $SendQueue = $line[2]
             $LocalAddress = $line[3]
             $ForeignAddress = $line[4]
             $State = $line[5]
 
-            $LocalPort = $($LocalAddress -Split '\.')[-1] # macOS uses '.' separator for port
+            # macOS uses '.' separator for port, replace it with ':'
+            $LocalAddress = $LocalAddress -Replace '(.+)\.(\d+)', '$1:$2'
+            $LocalPort = $($LocalAddress -Split ':')[-1]
 
             New-Object -TypeName PSObject -Property @{
                 Protocol = $Protocol
-                RecvQueue = $RecvQueue
-                SendQueue = $SendQueue
                 LocalAddress = $LocalAddress
                 LocalPort = $LocalPort
                 ForeignAddress = $ForeignAddress
@@ -65,7 +60,39 @@ function Get-Netstat
         }
     }
     else { # Windows
-        
+        # Windows netstat:
+        # Proto  Local Address          Foreign Address        State
+        # TCP    0.0.0.0:135            0.0.0.0:0              LISTENING
+        # TCP    192.168.25.132:4489    0.0.0.0:0              LISTENING
+
+        $output = netstat -an -p tcp | findstr LISTEN
+
+        foreach ($line in $output) {
+            $line = $line.Trim()
+            $line = $line -Split '\s+' # split line by whitespace
+            $Protocol = $line[0]
+            $LocalAddress = $line[1]
+            $ForeignAddress = $line[2]
+            $State = $line[3]
+
+            # Windows uses ':' separator for port
+            $LocalPort = $($LocalAddress -Split ':')[-1]
+
+            # Normalize TCP state names according to
+            # https://tools.ietf.org/html/rfc793#section-3.2
+
+            if ($State -eq 'LISTENING') {
+                $State = 'LISTEN' 
+            }
+
+            New-Object -TypeName PSObject -Property @{
+                Protocol = $Protocol
+                LocalAddress = $LocalAddress
+                LocalPort = $LocalPort
+                ForeignAddress = $ForeignAddress
+                State = $State
+            }
+        }
     }
 }
 
